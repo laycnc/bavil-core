8a67050d644340c728ebf57d6d7f3b3b7ed6a23e
laycnc <getsingleton@gmail.com>
2022-07-18 00:23:26 +0900

    テストコードを仮で追加

--- src/public/core/bavil_multicast_delegate.h
+++ src/public/core/bavil_multicast_delegate.h
@@ -50,6 +50,12 @@ namespace bavil
 	using function_t = std::function<void(Args...)>;
 
    public:
+	/**
+	 * @brief デリゲートを登録する
+	 * @tparam Func デリゲート型
+	 * @param func デリゲート
+	 * @return 
+	*/
 	template <class Func>
 	requires(std::constructible_from<function_t, Func>) delegate_handle
 	    add(Func func)
@@ -62,11 +68,30 @@ namespace bavil
 	  return handle;
 	}
 
+	/**
+	 * @brief デリゲートを削除する
+	 * @param _handle ハンドルの型
+	*/
 	void remove(delegate_handle _handle)
 	{
 	  m_events.erase(_handle);
 	}
 
+	bool is_valid( delegate_handle _handle ) const
+	{
+		auto result = m_events.find(_handle);
+		return result != m_events.end();
+	}
+
+	bool has_delegates() const
+	{
+		return !m_events.empty();
+	}
+
+	/**
+	 * @brief イベントの呼び出し
+	 * @param ...args 
+	*/
 	void broadcast(Args... args)
 	{
 	  for (auto& [handle, target] : m_events)
@@ -75,6 +100,9 @@ namespace bavil
 		}
 	}
 
+	/**
+	 * @brief 登録したイベントをクリアする
+	*/
 	void clear()
 	{
 	  m_events.clear();
---
+++ test/test_delegate.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include <core/bavil_multicast_delegate.h>
+
+// TESTマクロを使う場合
+
+namespace
+{
+	const char* SEND_MESSAGE = "[hogehoge]";
+}
+
+// 第1引数がテストケース名、第2引数がテスト名
+TEST(MyLibraryTest, Function1Test) {
+	//
+
+	bavil::multicast_delegate<void(const char* message)> events;
+
+	ASSERT_EQ(events.has_delegates(), false);
+
+	auto handle = events.add([](const char* _message)
+		{
+			ASSERT_STREQ(_message, SEND_MESSAGE);
+		});
+
+	ASSERT_EQ(events.has_delegates(), true);
+
+	events.broadcast(SEND_MESSAGE);
+
+	ASSERT_EQ(events.is_valid(handle), true);
+
+	events.remove(handle);
+
+	ASSERT_EQ(events.is_valid(handle), false);
+	ASSERT_EQ(events.has_delegates(), false);
+
+}
